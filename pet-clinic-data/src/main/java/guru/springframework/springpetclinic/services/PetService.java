5c99c043fc4affd5948995403b30e562711d517d
givemeanidea <byrne.dan01@gmail.com>
2019-07-21 11:39:07 +0100

    Adding bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/PetService.java
@@ -2,8 +2,6 @@ package guru.springframework.springpetclinic.services;
 
 import guru.springframework.springpetclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/VetService.java
@@ -2,8 +2,6 @@ package guru.springframework.springpetclinic.services;
 
 import guru.springframework.springpetclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet,Long> {
 
 }

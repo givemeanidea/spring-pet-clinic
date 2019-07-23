6823fe45b10ae79652d3ddb004b0bb9387cbbca3
Daniel <byrne.dan01@gmail.com>
2019-07-23 12:26:28 +0100

    Created Spring Data JPA Pet Specialty service. Closes #44

--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -3,10 +3,14 @@ package guru.springframework.springpetclinic.services.springdatajpa;
 import guru.springframework.springpetclinic.model.Pet;
 import guru.springframework.springpetclinic.repositories.PetRepository;
 import guru.springframework.springpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
     private final PetRepository petRepository;
--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/springdatajpa/PetTypeSDJpacService.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/springdatajpa/PetTypeSDJpacService.java
@@ -3,10 +3,14 @@ package guru.springframework.springpetclinic.services.springdatajpa;
 import guru.springframework.springpetclinic.model.PetType;
 import guru.springframework.springpetclinic.repositories.PetTypeRepository;
 import guru.springframework.springpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeSDJpacService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;

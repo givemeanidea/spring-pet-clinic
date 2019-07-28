0ccb310f505dd8d4f7675b5a221f6b710c19d17d
givemeanidea <byrne.dan01@gmail.com>
2019-07-28 12:25:18 +0100

    Implement Find Owner. Closes #54

--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package guru.springframework.springpetclinic.services;
 
 import guru.springframework.springpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/map/OwnerServiceMap.java
@@ -8,6 +8,7 @@ import guru.springframework.springpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -74,5 +75,12 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+
+        //todo IMPL
+        return null;
     }
 }
